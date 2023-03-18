014f532964d7adc178ae625c94a05eea43b5e187
astou-fall <128219411+astou-fall@users.noreply.github.com>
2023-03-18 12:18:39 +0000

    Add files via upload

---
+++ quiz.covid/PAGE QUIZZ COVID/scriptquiz.js
@@ -0,0 +1,65 @@
+
+window.addEventListener("DOMContentLoaded", (event) =>  {
+    const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (event) => {
+
+
+  let Totaloui = 0;
+  let Totalnon = 0;
+  const inputs = form.querySelectorAll('input[type="radio"]');
+  inputs.forEach((input) => {
+    if (input.checked) {
+      if (input.value === 'oui') {
+        Totaloui++;
+      } else if (input.value === 'non') {
+        Totalnon++;
+      }
+    }
+  });
+
+  
+  let Resultat;
+  if (Totaloui > Totalnon) {
+    result = 'ATTEINT';
+  } else {
+    result = 'PAS ATTEINT';
+  }
+
+  
+  alert(`VOUS ÊTES ${result} DE LA COVID-19.`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
